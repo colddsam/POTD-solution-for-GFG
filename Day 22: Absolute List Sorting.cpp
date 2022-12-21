5225db5981f9998723df0a7fa1300a1bd711e918
Samrat Kumar Das <90477654+Sam00036@users.noreply.github.com>
2022-12-21 15:17:11 +0530

    Create Day 22: Absolute List Sorting.cpp

---
+++ Day 22: Absolute List Sorting.cpp
@@ -0,0 +1,22 @@
+if(!head || !(head->next))
+            return head;
+        
+        Node *current=head->next;
+        Node *previous=head;
+        Node *front=head;
+        
+        while(current){
+            if((current->data)<0){
+                while(current && (current->data)<0){ 
+                    previous->next=current->next;
+                    current->next=front;
+                    front=current;
+                    current=previous->next;
+                }
+            }
+            if(current){
+                current=current->next;
+                previous=previous->next;
+            }
+        }
+        return front;
