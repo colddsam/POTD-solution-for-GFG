d25ea9db9fcdb21a09f9cd8b79d05f78e1512623
Samrat Kumar Das <90477654+Sam00036@users.noreply.github.com>
2022-12-31 20:55:11 +0530

    Create Day 31: Single valued subtree.cpp

---
+++ Day 31: Single valued subtree.cpp
@@ -0,0 +1,34 @@
+//User function Template for C++
+
+/* Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};*/
+
+class Solution
+{
+    public:
+    int ans=0;
+    pair<bool,int> subtree(Node* root){
+        if(!root){
+            return {true,INT_MAX};
+        }
+        pair<bool,int>left=subtree(root->left);
+        pair<bool,int>right=subtree(root->right);
+        
+        if(left.first && right.first){
+            if((root->data==left.second || left.second==INT_MAX) && (root->data==right.second || right.second==INT_MAX)){
+                 ans++;
+                 return {true,root->data};
+            }
+        }
+        return {false,root->data};
+    }
+    int singlevalued(Node *root){
+        pair<bool,int> temp=subtree(root);
+        return ans;
+    }
+};
