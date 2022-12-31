dc9b1189b24a9ba1e41c670a8cef020517426350
Samrat Kumar Das <90477654+Sam00036@users.noreply.github.com>
2022-12-31 20:53:24 +0530

    Create Day 28: Container With Most Water.cpp

---
+++ Day 28: Container With Most Water.cpp
@@ -0,0 +1,16 @@
+//User function template for C++
+long long maxArea(long long arr[], int len)
+{
+    long long ans = 0;
+    int low = 0, high = len-1;
+    while(low < high){
+        long long water = (high - low) * min(arr[high], arr[low]);
+        if(arr[low] < arr[high])
+            low++;
+        else
+            high--;
+            
+        ans = max(ans ,water);
+    }
+    return ans;
+}
