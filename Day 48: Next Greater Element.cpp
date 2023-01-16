5f660e95a5509f4acfcce624a1062c7d3299ce7e
Samrat Kumar Das <90477654+Sam00036@users.noreply.github.com>
2023-01-16 16:31:58 +0530

    Create Day 48: Next Greater Element.cpp

---
+++ Day 48: Next Greater Element.cpp
@@ -0,0 +1,13 @@
+vector<long long> ans(n, -1);
+        stack<long long> st;
+        st.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--){
+            while(!st.empty() && arr[i]>=st.top()){
+                st.pop();
+            }   
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return ans;
