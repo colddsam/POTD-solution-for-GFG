14fc5a4eb77f6b6c052db8841a8a96fcc4679a2a
Samrat Kumar Das <90477654+Sam00036@users.noreply.github.com>
2023-02-04 21:24:48 +0530

    Create Day 67: Max Sum without Adjacents.cpp

---
+++ Day 67: Max Sum without Adjacents.cpp
@@ -0,0 +1,9 @@
+int findMaxSum(int *arr, int n) {
+	    int ans=0,mx=0;
+        for(int i=0;i<n;i++) {
+            int val = arr[i]+mx;
+            mx = max(mx, ans);
+            ans = max(ans,val);
+        }
+        return ans;
+	}
