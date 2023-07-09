911a889bb07fb380a64549c2100a5dd21d08168b
Samrat Kumar Das <90477654+colddsam@users.noreply.github.com>
2023-07-09 06:05:07 +0530

    Create Day 141: Smallest Positive missing number.cpp

---
+++ Day 141: Smallest Positive missing number.cpp
@@ -0,0 +1,13 @@
+int missingNumber(int A[], int n) 
+    { 
+        for(int i = 0; i < n; ++ i) {
+            while(A[i] >= 1 && A[i] <= n && A[i] != A[A[i]-1])
+                swap(A[i], A[A[i] - 1]);
+        }
+        
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
+    } 
