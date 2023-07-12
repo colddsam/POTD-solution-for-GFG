8a1b75d4e121cb19504f9dca223023f723732e25
Samrat Kumar Das <90477654+colddsam@users.noreply.github.com>
2023-07-12 22:06:47 +0530

    Create Day 144: Power Of Numbers.cpp

---
+++ Day 144: Power Of Numbers.cpp
@@ -0,0 +1,21 @@
+long power(int n,int p)
+    {
+        //Your code here
+        int mod = 1000000007;
+        
+        if(p==0)
+        return 1;
+        
+        long ans=0;
+        if(p%2 == 0){
+            ans = power(n,p/2);
+            ans = ( ans * ans) %mod;
+        }
+        
+        else{
+        ans = n%mod;
+        ans =  ( ans * power(n ,p-1) %mod ) % mod;
+        }
+        
+        return (ans)%mod;
+    }
