96f95dd4765814980545fb420bc4dab693f3de54
Samrat Kumar Das <90477654+colddsam@users.noreply.github.com>
2023-10-01 21:31:57 +0530

    Create Day 221: Boundary traversal of matrix.cpp

---
+++ Day 221: Boundary traversal of matrix.cpp
@@ -0,0 +1,15 @@
+ class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+     vector<int> boundaryTraversal(vector<vector<int>>& matrix, int n, int m) {
+        vector<int> result;
+        for (int i = 0; i < m; i++) result.push_back(matrix[0][i]);
+        for (int i = 1; i < n; i++) result.push_back(matrix[i][m - 1]);
+        if (n > 1) for (int i = m - 2; i >= 0; i--) result.push_back(matrix[n - 1][i]);
+        if (m > 1) for (int i = n - 2; i > 0; i--) result.push_back(matrix[i][0]);
+
+        return result;
+    }
+};
